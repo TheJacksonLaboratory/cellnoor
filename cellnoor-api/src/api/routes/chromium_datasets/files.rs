1450b2f474bfea4f52f62e2a314115002fa1af5f
Ahmed Said <ahmed.said@jax.org>
2026-03-22 11:55:52 -0400

    make body size limit much greater

--- cellnoor-api/src/api/routes/chromium_datasets/files.rs
+++ cellnoor-api/src/api/routes/chromium_datasets/files.rs
@@ -15,10 +15,14 @@ mod download;
 mod upload;
 
 pub(super) fn router() -> Router<AppState> {
-    const BODY_LIMIT_128MB: usize = 128_000_000;
+    const WEB_SUMMARY_FILE_SIZE: usize = 16 * 1_000_000; // 16 MB
+    const MAX_N_SAMPLES: usize = 384;
+
+    // Each sample has its own web_summary.html
+    const BODY_SIZE_LIMIT: usize = WEB_SUMMARY_FILE_SIZE * MAX_N_SAMPLES;
 
     let file_upload_layer =
-        admin_required_creation!().layer(DefaultBodyLimit::max(BODY_LIMIT_128MB));
+        admin_required_creation!().layer(DefaultBodyLimit::max(BODY_SIZE_LIMIT));
 
     Router::new()
         .route("/files/", post(upload_file).layer(file_upload_layer))
