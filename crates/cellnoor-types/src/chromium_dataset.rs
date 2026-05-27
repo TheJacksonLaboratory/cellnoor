c58ce19846fdf14948d53abc0b3f672a0435cd1c
Ahmed Said <ahmed.said@jax.org>
2026-05-27 17:22:05 -0400

    add chromium dataset type

--- crates/cellnoor-types/src/chromium_dataset.rs
+++ crates/cellnoor-types/src/chromium_dataset.rs
@@ -1,4 +1,35 @@
-use macro_attributes::unit_enum;
+use macro_attributes::{base_model, unit_enum};
+use nonempty::NonemptyVec;
+pub use query::{
+    ChromiumDatasetField, ChromiumDatasetPredicate, ChromiumDatasetPredicateInner,
+    ChromiumDatasetQuery, SimpleChromiumDatasetQuery,
+};
+use uuid::Uuid;
+
+use crate::{
+    chromium_dataset::record::ChromiumDatasetRecord,
+    id::{Id, NoId},
+    library::LibraryCompact,
+    simple_links::SimpleLinks,
+    suspension_pool::TaggedSpecimen,
+};
+
+mod query;
+
+mod record {
+    use jiff::Timestamp;
+    use macro_attributes::select;
+    use nonempty::NonemptyString;
+
+    #[select]
+    #[cfg_attr(feature = "postgres-types", postgres(name = "chromium_dataset"))]
+    pub struct ChromiumDatasetRecord<T> {
+        #[cfg_attr(feature = "serde", serde(flatten))]
+        pub id: T,
+        pub name: NonemptyString,
+        pub delivered_at: Timestamp,
+    }
+}
 
 #[unit_enum]
 pub enum ChromiumDatasetCmdline {
@@ -17,4 +48,35 @@ pub enum ChromiumDatasetCmdline {
     #[serde(rename = "cellranger vdj")]
     #[strum(serialize = "cellranger vdj")]
     CellrangerVdj,
+}
+
+pub type NewChromiumDatasetRecord = ChromiumDatasetRecord<NoId>;
+
+pub type SavedChromiumDatasetRecord = ChromiumDatasetRecord<Id>;
+
+pub type ChromiumDatasetUpdate = NewChromiumDatasetRecord;
+
+#[base_model]
+pub struct NewChromiumDataset {
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub record: NewChromiumDatasetRecord,
+    pub library_ids: NonemptyVec<Uuid>,
+    pub cmdline: ChromiumDatasetCmdline,
+}
+
+#[base_model]
+pub struct ChromiumDatasetCompact {
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub record: SavedChromiumDatasetRecord,
+    pub links: SimpleLinks,
+}
+
+#[base_model]
+pub struct ChromiumDatasetDetailed {
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub record: SavedChromiumDatasetRecord,
+    pub links: SimpleLinks,
+    pub libraries: Vec<LibraryCompact>,
+    pub specimens: Vec<TaggedSpecimen>,
+    pub raw_file_paths: Vec<String>,
 }
---
+++ crates/cellnoor-types/src/chromium_dataset/query.rs
@@ -0,0 +1,87 @@
+use macro_attributes::{base_model, predicate_enum, sort_field_enum};
+
+use crate::{
+    library::LibraryPredicate,
+    operator::{StringOperator, TimestampOperator, UuidOperator},
+    query::{ComplexQuery, SimpleQuery, filter::Filter},
+    specimen::SpecimenPredicate,
+    tenx_assay::TenxAssayPredicate,
+};
+
+#[predicate_enum]
+#[strum(prefix = "(chromium_dataset).")]
+#[strum_discriminants(
+    name(ChromiumDatasetField),
+    sort_field_enum,
+    strum(prefix = "(chromium_dataset).")
+)]
+pub enum ChromiumDatasetPredicateInner {
+    Id(UuidOperator),
+    Name(StringOperator),
+    DeliveredAt(TimestampOperator),
+}
+
+#[base_model]
+#[derive(strum::IntoStaticStr)]
+pub enum ChromiumDatasetPredicate {
+    #[strum(transparent)]
+    Specimen(SpecimenPredicate),
+    #[strum(transparent)]
+    TenxAssay(TenxAssayPredicate),
+    #[strum(transparent)]
+    Library(LibraryPredicate),
+    #[cfg_attr(feature = "serde", serde(untagged))]
+    #[strum(transparent)]
+    ChromiumDataset(ChromiumDatasetPredicateInner),
+}
+
+impl ChromiumDatasetPredicate {
+    pub fn field_name(&self) -> &'static str {
+        match self {
+            Self::Specimen(p) => p.field_name(),
+            Self::TenxAssay(p) => p.field_name(),
+            Self::Library(p) => p.field_name(),
+            Self::ChromiumDataset(p) => p.field_name(),
+        }
+    }
+}
+
+impl From<SpecimenPredicate> for ChromiumDatasetPredicate {
+    fn from(value: SpecimenPredicate) -> Self {
+        Self::Specimen(value)
+    }
+}
+
+impl From<TenxAssayPredicate> for ChromiumDatasetPredicate {
+    fn from(value: TenxAssayPredicate) -> Self {
+        Self::TenxAssay(value)
+    }
+}
+
+impl From<LibraryPredicate> for ChromiumDatasetPredicate {
+    fn from(value: LibraryPredicate) -> Self {
+        Self::Library(value)
+    }
+}
+
+impl From<ChromiumDatasetPredicateInner> for ChromiumDatasetPredicate {
+    fn from(value: ChromiumDatasetPredicateInner) -> Self {
+        Self::ChromiumDataset(value)
+    }
+}
+
+impl From<ChromiumDatasetPredicateInner> for Filter<ChromiumDatasetPredicate> {
+    fn from(value: ChromiumDatasetPredicateInner) -> Self {
+        Self::Leaf(value.into())
+    }
+}
+
+impl Default for ChromiumDatasetField {
+    fn default() -> Self {
+        Self::DeliveredAt
+    }
+}
+
+pub type ChromiumDatasetQuery = ComplexQuery<ChromiumDatasetPredicate, ChromiumDatasetField>;
+
+pub type SimpleChromiumDatasetQuery = SimpleQuery<ChromiumDatasetField>;
