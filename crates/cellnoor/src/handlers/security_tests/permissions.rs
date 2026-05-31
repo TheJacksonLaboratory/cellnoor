0d73d33f65ae960800fe26ba38ce51353462b74c
Ahmed Said <ahmed.said@jax.org>
2026-05-30 21:55:42 -0400

    add perm test

--- crates/cellnoor/src/handlers/security_tests/permissions.rs
+++ crates/cellnoor/src/handlers/security_tests/permissions.rs
@@ -6,8 +6,16 @@ use crate::{
     db,
     error::ErrorInner,
     handlers::{
+        cdna::show::select_cdna_by_id,
+        chromium_datasets::show::select_chromium_dataset_by_id,
+        chromium_runs::show::select_chromium_run_by_id,
         institutions::show::select_institution_by_id,
-        people::create::test::insert_test_person_and_institution,
+        libraries::show::select_library_by_id,
+        people::{create::test::insert_test_person_and_institution, show::select_person_by_id},
+        projects::show::select_project_by_id,
+        specimens::show::select_specimen_by_id,
+        suspension_pools::show::select_suspension_pool_by_id,
+        suspensions::show::select_suspension_by_id,
     },
     state::test_util::{db_client_as_admin, db_client_as_user},
 };
@@ -25,16 +33,14 @@ async fn create_test_user() -> Uuid {
     *person.record.id
 }
 
-async fn test_no_error<F, T>(tx: &db::Transaction<'_>, select_fn: F)
+async fn assert_no_resource_found<F, T>(tx: &db::Transaction<'_>, select_fn: F)
 where
     T: Debug,
     F: AsyncFn(&db::Transaction, Uuid) -> Result<T, ErrorInner>,
 {
-    static RANDOM_ID: Uuid = Uuid::max();
-
     // In this test, we just want to see that we don't get a PermissionDenied
-    let err = select_fn(tx, RANDOM_ID).await.unwrap_err();
-    pretty_assertions::assert_eq!(err, ErrorInner::ResourceNotFound)
+    let err = select_fn(tx, Uuid::max()).await.unwrap_err();
+    pretty_assertions::assert_eq!(err, ErrorInner::ResourceNotFound,)
 }
 
 #[tokio::test(flavor = "multi_thread")]
@@ -44,5 +50,17 @@ async fn user_can_access_every_view() {
     let mut client = db_client_as_user(user_id).await;
     let tx = &client.begin().await.unwrap();
 
-    tokio::join!(test_no_error(tx, select_institution_by_id));
+    tokio::join!(
+        assert_no_resource_found(tx, select_cdna_by_id),
+        assert_no_resource_found(tx, async |tx, id| select_chromium_dataset_by_id(tx, "", id)
+            .await),
+        assert_no_resource_found(tx, select_chromium_run_by_id),
+        assert_no_resource_found(tx, select_institution_by_id),
+        assert_no_resource_found(tx, select_library_by_id),
+        assert_no_resource_found(tx, select_person_by_id),
+        assert_no_resource_found(tx, select_project_by_id),
+        assert_no_resource_found(tx, select_specimen_by_id),
+        assert_no_resource_found(tx, select_suspension_by_id),
+        assert_no_resource_found(tx, select_suspension_pool_by_id),
+    );
 }
