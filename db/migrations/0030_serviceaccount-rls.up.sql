d68223bd5ee4aaf889346423fd894d53e4e23eb0
Ahmed Said <ahmed.said@jax.org>
2026-06-04 18:08:47 -0400

    fix misplaced function

--- db/migrations/0030_serviceaccount-rls.up.sql
+++ db/migrations/0030_serviceaccount-rls.up.sql
@@ -1,3 +1,16 @@
+create or replace function current_user_has_access_to_service_account(
+    service_account_id_to_check uuid
+) returns boolean language plpgsql volatile strict as $$
+    declare
+        has_access boolean;
+        current_user_id uuid = current_user::uuid;
+    begin
+        select exists (select 1 from service_account_access where current_user_id = service_account_access.person_id and service_account_access.service_account_id = service_account_id_to_check) into has_access;
+
+        return has_access;
+    end;
+$$;
+
 alter table service_account enable row level security;
 
 create policy owner_can_insert_service_account on service_account for insert with check (owned_by = current_user::uuid);
