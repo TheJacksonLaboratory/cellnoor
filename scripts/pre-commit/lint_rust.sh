2efe732b63fa54906299421362028a506b5db940
Ahmed Said <ahmed.said@jax.org>
2026-08-20 18:30:34 -0400

    remove unnecessary pre-commit BS and move  module into separate file

--- scripts/pre-commit/lint_rust.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-DATABASE_URL="postgres://postgres@localhost:5432/cellnoor-compilation" cargo clippy --fix --allow-dirty --workspace --all-features
-cargo +nightly fmt
